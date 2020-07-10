d83554f519cdbf46da0d24994751378653318b63
Ashley Mills <ashleymills@mac.com>
2020-07-10 17:26:27 +0100

    Make package dynamic

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SemanticVersion", targets: ["SemanticVersion"]),
+            name: "SemanticVersion", type: .dynamic, targets: ["SemanticVersion"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
